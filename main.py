7c1c78d9c25499119c5833dc9f518572fe66356c
swathikannepalli <99465513+swathikannepalli@users.noreply.github.com>
2022-09-08 13:23:44 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import streamlit as st
+st.header("house rent predication")
+df = pd.read_csv(r'C:\\Users\\swath\OneDrive\Desktop\House_Rent_Dataset.csv')
+st.write(df)
+genre = st.radio(
+     "What's your favorite movie genre",
+     ('a', 'b', 'c'))
+if genre == 'a':
+    st.table(df)
+    st.write('now u can see the table')
+elif genre == 'b':
+    st.bar_chart(data=df, x='City', y='Rent', width=0, height=0, use_container_width=True)
+    st.write('your analysis is ready based on city')
+elif genre == 'c':
+    st.line_chart(data=df, x='City', y='Size', width=0, height=0, use_container_width=True)
+    st.write('your analysis is ready based on size')
+else:
+    st.write('get out')
